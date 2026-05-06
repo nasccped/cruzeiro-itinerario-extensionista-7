140dd3c98adf54b836d2cc70d89140d5038cded1
Nasccped <pedro.main.tec@gmail.com>
2026-05-05 22:38:08 -0300

    chore: setup initial server

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,7 +1,15 @@
-fn turn_green(x: &str) -> String {
-    format!("\x1b[92m{}\x1b[0m", x)
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, main as async_main};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Olá!")
 }
 
-fn main() {
-    println!("Server is {}!", turn_green("ready to run"));
+#[async_main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(home))
+        .bind(("127.0.0.1", 8080))
+        .unwrap_or_else(|e| panic!("expected success but http server bound returned: {e}"));
+    println!("server successfully bound to 127.0.0.1:8080 :^D");
+    server.run().await
 }
