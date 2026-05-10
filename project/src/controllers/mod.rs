4437dbef451152c3e39926a497fe6558549377b5
Nasccped <pedro.main.tec@gmail.com>
2026-05-10 11:48:30 -0300

    chore: add controllers

--- project/src/controllers/mod.rs
+++ project/src/controllers/mod.rs
@@ -1,3 +1,28 @@
 mod user_controllers;
 
-pub use user_controllers::UserController;
+use actix_web::{HttpResponse, Responder};
+use std::collections::HashSet;
+pub use user_controllers::UserControllers;
+
+/// Retorna o endpoint para [`app_home`].
+pub fn get_app_home_endpoint() -> &'static str {
+    "/"
+}
+
+/// Rota para o endpoint vazio (`/`).
+pub async fn app_home() -> impl Responder {
+    let mut response = "URL inicial. Considere utilizar os demais endpoints:\n\n".to_string();
+    let endpoints: HashSet<&str> = [
+        UserControllers::get_users_endpoint(),
+        UserControllers::get_user_by_id_endpoint(),
+    ]
+    .into();
+    response.push_str(
+        &endpoints
+            .iter()
+            .map(|edp| format!("- {}", edp))
+            .collect::<Vec<_>>()
+            .join("\n"),
+    );
+    HttpResponse::Ok().body(response)
+}
--- project/src/controllers/user_controllers.rs
+++ project/src/controllers/user_controllers.rs
@@ -1,16 +1,31 @@
-use actix_web::{HttpResponse, Responder, web::Path};
+use crate::usecases::UserUsecases;
+use actix_web::{HttpResponse, Responder, web};
 
-/// Controller para as operações com os usuários.
-pub struct UserController {}
+pub struct UserControllers {}
 
-impl UserController {
+impl UserControllers {
+    /// Retorna o endpoint para [`UserControllers::get_users`].
+    pub fn get_users_endpoint() -> &'static str {
+        "/users"
+    }
+
+    /// Retorna o endpoint para [`UserControllers::get_user_by_id`].
+    pub fn get_user_by_id_endpoint() -> &'static str {
+        "/users/{userId}"
+    }
+
     /// Retorna uma lista com todos os usuários.
-    pub async fn get_users() -> impl Responder {
-        HttpResponse::Ok().body("Vamos retornar alguns usuários")
+    pub async fn get_users(usecase: web::Data<UserUsecases>) -> impl Responder {
+        let response = usecase.get_users().await;
+        HttpResponse::Ok().body(response.to_string())
     }
 
     /// Retorna o usuário especificado pelo id.
-    pub async fn get_user_by_id(user_id: Path<String>) -> impl Responder {
-        HttpResponse::Ok().body(format!("Retornando usuário de id {}", user_id))
+    pub async fn get_user_by_id(
+        usecase: web::Data<UserUsecases>,
+        user_id: web::Path<String>,
+    ) -> impl Responder {
+        let response = usecase.get_user_by_id(user_id).await;
+        HttpResponse::Ok().body(response)
     }
 }
