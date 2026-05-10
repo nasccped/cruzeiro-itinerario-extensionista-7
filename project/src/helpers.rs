9be410753da6f327b13db54465646de7cbf13267
Nasccped <pedro.main.tec@gmail.com>
2026-05-10 11:48:55 -0300

    chore: add helpers + setup main file

---
+++ project/src/helpers.rs
@@ -0,0 +1,71 @@
+use crate::{
+    config::Config,
+    controllers::{self, UserControllers},
+};
+use actix_web::{
+    App, Error,
+    body::MessageBody,
+    dev::{ServiceFactory, ServiceRequest, ServiceResponse},
+    web,
+};
+use std::fmt::{Debug, Display};
+
+/// Tenta executar [`dotenv::dotenv`]. Executa [`panic`] caso haja falha.
+pub fn setup_dotenv() {
+    dotenv::dotenv().unwrap_or_else(|e| panic!("Falha ao executar função `dotenv`: {e:?}"));
+}
+
+/// Inicializa o logger.
+pub fn init_logger() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("project=info"))
+        .init();
+}
+
+/// Função de [`panic`] para quando variável de ambiente não é definida.
+pub fn undefined_var_panic(name: impl Display) -> ! {
+    panic!("Variável de ambiente ${name} não definida");
+}
+
+/// Função de [`panic`] para quando variável de ambiente possui valor inválido.
+pub fn invalid_var_format_panic(name: impl Display, value: impl Display) -> ! {
+    panic!("Variável de ambiente ${name} possui valor inválido: {value}");
+}
+
+/// Quando a construção do [`actix_web::HttpServer`] retorna [`Err`].
+pub fn could_not_build_server(err: impl Debug) -> ! {
+    panic!("Não foi possível gerar o servidor: {err:?}");
+}
+
+/// Cria um novo [`App`] padrão para o projeto.
+pub fn create_app(
+    config: web::Data<Config>,
+) -> App<
+    impl ServiceFactory<
+        ServiceRequest,
+        Response = ServiceResponse<impl MessageBody>,
+        Config = (),
+        InitError = (),
+        Error = Error,
+    >,
+> {
+    let user_usecases = config.user_usecases.clone();
+    let mut app = App::new().app_data(user_usecases).app_data(config.clone());
+    let endpoints_and_routes = [
+        (
+            controllers::get_app_home_endpoint(),
+            web::get().to(controllers::app_home),
+        ),
+        (
+            UserControllers::get_users_endpoint(),
+            web::get().to(UserControllers::get_users),
+        ),
+        (
+            UserControllers::get_user_by_id_endpoint(),
+            web::get().to(UserControllers::get_user_by_id),
+        ),
+    ];
+    for (endpoint, route) in endpoints_and_routes {
+        app = app.route(endpoint, route);
+    }
+    app
+}
--- project/src/main.rs
+++ project/src/main.rs
@@ -1,46 +1,24 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, main as async_main};
-use dotenv::dotenv;
-use std::env;
+mod config;
+mod controllers;
+mod helpers;
+mod usecases;
 
-const SERVER_URL: &str = "SERVER_URL";
-const SERVER_PORT: &str = "SERVER_PORT";
+use actix_web::{HttpServer, web};
+use config::Config;
 
-/// Executa [`panic`] quando não é possível ler a variável de ambiente.
-fn unreadable_var_panic(var_name: &str) -> ! {
-    panic!("Não foi possível ler a varíavel ${}", var_name);
-}
-
-/// Retorna a URL do servidor.
-fn get_server_url() -> String {
-    env::var(SERVER_URL).unwrap_or_else(|_| unreadable_var_panic(SERVER_URL))
-}
-
-/// Retorna a porta do servidor.
-fn get_server_port() -> u16 {
-    let port = env::var(SERVER_PORT).unwrap_or_else(|_| unreadable_var_panic(SERVER_PORT));
-    if let Ok(p) = port.parse() {
-        p
-    } else {
-        panic!(
-            "o variável ${} não possui um valor apropriado: {}",
-            SERVER_PORT, port
-        );
-    }
-}
-
-#[get("/")]
-async fn home() -> impl Responder {
-    HttpResponse::Ok().body("Olá!")
-}
-
-#[async_main]
+#[actix_web::main]
 async fn main() {
-    dotenv().ok();
-    let server_url = get_server_url();
-    let server_port = get_server_port();
-    let server = HttpServer::new(|| App::new().service(home))
-        .bind((server_url.clone(), server_port))
-        .unwrap_or_else(|e| panic!("Falha ao gerar o servidor: {:?}", e));
-    println!("Servidor rodando em `{}:{}`", server_url, server_port);
+    helpers::setup_dotenv();
+    helpers::init_logger();
+    let conf = web::Data::new(Config::default());
+    let clone = conf.clone();
+    let server = HttpServer::new(move || helpers::create_app(clone.clone()))
+        .bind((conf.get_server_url(), conf.get_server_port()))
+        .unwrap_or_else(|e| helpers::could_not_build_server(e));
+    log::info!(
+        "Servidor rodando em `{}:{}`",
+        conf.get_server_url(),
+        conf.get_server_port()
+    );
     server.run().await.unwrap()
 }
