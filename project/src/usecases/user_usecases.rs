0fa4bba5dcdfd54fc71e8ea3a6433b41ae307ca3
Nasccped <pedro.main.tec@gmail.com>
2026-05-13 23:16:17 -0300

    fix: `get_users` usecase change

--- project/src/usecases/user_usecases.rs
+++ project/src/usecases/user_usecases.rs
@@ -1,14 +1,44 @@
-use crate::models::users::{User, Users};
+use crate::{
+    models::{
+        ModelParseError,
+        users::{User, Users},
+    },
+    repositories::UserRepository,
+};
 use actix_web::web;
+use sqlx::{FromRow, PgPool};
 
 /// Casos de uso para as operações com os usuários.
-#[derive(Default)]
-pub struct UserUsecases {}
+pub struct UserUsecases {
+    repository: UserRepository,
+}
 
 impl UserUsecases {
+    /// Cria uma nova instância de [`UserUsecases`] a partir de uma conexão postgres já
+    /// estabelecida.
+    pub fn new(conn: PgPool) -> Self {
+        let repository = UserRepository::from(conn);
+        Self { repository }
+    }
+
     /// Retorna uma lista com todos os usuários.
-    pub async fn get_users(&self) -> Users {
-        (0..3).map(|_| User::default()).collect()
+    pub async fn get_users(&self) -> Result<Users, String> {
+        let result = self
+            .repository
+            .get_users()
+            .await
+            .map_err(|e| e.to_string())?;
+        let mut users = Users::empty();
+        for row in result {
+            let user = User::from_row(&row).map_err(|e| {
+                ModelParseError::from(e)
+                    .with_model_name::<User>()
+                    .with_input(row)
+                    .to_string()
+            })?;
+            users.push(user);
+        }
+        Ok(users)
     }
 
     /// Retorna o usuário especificado pelo id.
