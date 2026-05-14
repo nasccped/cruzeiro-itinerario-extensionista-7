5f642a43bed4c76a092cd616a2e35422c9fe66cf
Nasccped <pedro.main.tec@gmail.com>
2026-05-13 23:12:40 -0300

    chore: add model parsing common error

--- project/src/models/mod.rs
+++ project/src/models/mod.rs
@@ -1,1 +1,48 @@
 pub mod users;
+
+use sqlx::Error;
+use std::{any::Any, fmt::Debug};
+
+/// Erro quando parse de struct falha.
+pub struct ModelParseError<D: Debug> {
+    model_name: Option<&'static str>,
+    input: Option<D>,
+    err: Error,
+}
+
+impl<D: Debug> From<Error> for ModelParseError<D> {
+    fn from(value: Error) -> Self {
+        Self {
+            model_name: None,
+            input: None,
+            err: value,
+        }
+    }
+}
+
+impl<D: Debug> ModelParseError<D> {
+    /// Especifica o nome do model onde o parser falhou.
+    pub fn with_model_name<T: Any>(mut self) -> Self {
+        self.model_name = Some(std::any::type_name::<T>());
+        self
+    }
+
+    /// Especifica o input utilizado (debug mode) durante a falha do parsing.
+    pub fn with_input(mut self, input: D) -> Self {
+        self.input = Some(input);
+        self
+    }
+}
+
+#[allow(clippy::to_string_trait_impl)]
+impl<D: Debug> ToString for ModelParseError<D> {
+    fn to_string(&self) -> String {
+        format!(
+            "Não for possível parsear o objeto `{}` por meio do input => {:?}.\n\
+            Tentativa resulta em => {}",
+            self.model_name.unwrap_or("<INDEFINIDO>"),
+            self.input,
+            self.err
+        )
+    }
+}
