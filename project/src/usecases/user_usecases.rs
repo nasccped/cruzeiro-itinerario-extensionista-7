dbee35c9860bc62d5d023972e173009027be6d5d
Nasccped <pedro.main.tec@gmail.com>
2026-05-15 21:49:09 -0300

    chore: do users fetching + context error enum

--- project/src/usecases/user_usecases.rs
+++ project/src/usecases/user_usecases.rs
@@ -1,12 +1,13 @@
+#![allow(clippy::from_over_into)]
 use crate::{
     models::{
-        ModelParseError,
+        error::ModelParseError,
         users::{User, Users},
     },
-    repositories::UserRepository,
+    repositories::{UserRepository, error::DbOperationError},
 };
-use actix_web::web;
-use sqlx::{FromRow, PgPool};
+use actix_web::HttpResponse;
+use sqlx::{FromRow, PgPool, postgres::PgRow};
 
 /// Casos de uso para as operações com os usuários.
 pub struct UserUsecases {
@@ -22,19 +23,16 @@ impl UserUsecases {
     }
 
     /// Retorna uma lista com todos os usuários.
-    pub async fn get_users(&self) -> Result<Users, String> {
+    pub async fn get_users(&self) -> Result<Users, GetUsersError> {
         let result = self
             .repository
             .get_users()
             .await
-            .map_err(|e| e.to_string())?;
+            .map_err(GetUsersError::DbQuery)?;
         let mut users = Users::empty();
         for row in result {
             let user = User::from_row(&row).map_err(|e| {
-                ModelParseError::from(e)
-                    .with_model_name::<User>()
-                    .with_input(row)
-                    .to_string()
+                GetUsersError::UserParsing(ModelParseError::from_err_and_input(e, row))
             })?;
             users.push(user);
         }
@@ -42,7 +40,71 @@ impl UserUsecases {
     }
 
     /// Retorna o usuário especificado pelo id.
-    pub async fn get_user_by_id(&self, user_id: web::Path<String>) -> String {
-        format!("Retornando usuário de id {}", user_id)
+    pub async fn get_user_by_id(&self, id: &str) -> Result<User, GetUserByIdError> {
+        let id = id
+            .parse()
+            .map_err(|_| GetUserByIdError::InvalidId(id.into()))?;
+        let pg_row = self
+            .repository
+            .get_user_by_id(id)
+            .await
+            .map_err(GetUserByIdError::DbQuery)?
+            .ok_or(GetUserByIdError::NotFound(id))?;
+        User::from_row(&pg_row).map_err(|e| {
+            GetUserByIdError::UserParsing(ModelParseError::from_err_and_input(e, pg_row))
+        })
+    }
+}
+
+/// Possíveis erros para retorno de [`UserUsecases::get_users`].
+pub enum GetUsersError {
+    /// Quando o erro ocorre na execução da query.
+    DbQuery(DbOperationError),
+    /// Quando o erro ocorre na deserialização da [`PgRow`].
+    UserParsing(ModelParseError<User, PgRow>),
+}
+
+impl Into<HttpResponse> for GetUsersError {
+    fn into(self) -> HttpResponse {
+        match self {
+            Self::DbQuery(err) => err.into(),
+            Self::UserParsing(err) => err.into(),
+        }
+    }
+}
+
+/// Possíveis erros para o retorno de [`UserUsecases::get_user_by_id`].
+pub enum GetUserByIdError {
+    /// Quando o id é inválido.
+    InvalidId(String),
+    /// Quando usuário de id especificado não existe.
+    NotFound(i128),
+    /// Quando o erro ocorre na execução da query.
+    DbQuery(DbOperationError),
+    /// Quando o erro ocorre na deserialização da [`PgRow`].
+    UserParsing(ModelParseError<User, PgRow>),
+}
+
+impl Into<HttpResponse> for GetUserByIdError {
+    fn into(self) -> HttpResponse {
+        let s = self.to_string();
+        match self {
+            Self::InvalidId(_) => HttpResponse::BadRequest().body(s),
+            Self::NotFound(_) => HttpResponse::NotFound().body(s),
+            Self::DbQuery(err) => err.into(),
+            Self::UserParsing(err) => err.into(),
+        }
+    }
+}
+
+#[allow(clippy::to_string_trait_impl)]
+impl ToString for GetUserByIdError {
+    fn to_string(&self) -> String {
+        match self {
+            Self::InvalidId(id) => format!("`{}` não é reconhecido como um id válido.", id),
+            Self::NotFound(id) => format!("O usuário de id `{}` não foi encontrado.", id),
+            Self::DbQuery(err) => err.to_string(),
+            Self::UserParsing(err) => err.to_string(),
+        }
     }
 }
