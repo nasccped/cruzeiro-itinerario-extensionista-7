9fef33d80ff8936d2accf2945ae98177c2c9efab
Nasccped <pedro.main.tec@gmail.com>
2026-05-17 16:36:28 -0300

    chore: add post moderator to controller

--- project/src/controllers/moderator.rs
+++ project/src/controllers/moderator.rs
@@ -13,11 +13,27 @@ impl ModeratorController {
         MODERATORS_ENDPOINT
     }
 
+    /// Retorna o endpoint para [`ModeratorController::post_moderator`].
+    pub fn post_moderator_endpoint() -> &'static str {
+        MODERATORS_ENDPOINT
+    }
+
     /// Retorna uma lista contendo todos os moderadores.
     pub async fn get_moderators(usecase: web::Data<ModeratorUsecase>) -> HttpResponse {
         utils::log_and_normalize(
             usecase.get_moderators().await,
             Self::get_moderators_endpoint(),
+        )
+    }
+
+    /// Tenta inserir um novo moderador na tabela de moderadores.
+    pub async fn post_moderator(
+        usecase: web::Data<ModeratorUsecase>,
+        body: String,
+    ) -> HttpResponse {
+        utils::log_and_normalize(
+            usecase.post_moderator(body).await,
+            Self::post_moderator_endpoint(),
         )
     }
 }
