3391a73db645ca3b2cbed67fc3b3091cecb4a5a8
Nasccped <pedro.main.tec@gmail.com>
2026-05-17 14:53:06 -0300

    chore: add status default + mod. insert function

--- project/db-templates/default-schema.sql
+++ project/db-templates/default-schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
   user_name      VARCHAR(50) NOT NULL UNIQUE,
   user_mail      VARCHAR(50) NOT NULL UNIQUE,
   latest_change  TIMESTAMPTZ                ,
-  current_status USER_STATUS
+  current_status USER_STATUS DEFAULT 'available'
 );
 
 -- tabela para os moderadores
@@ -36,3 +36,43 @@ CREATE VIEW moderators_view AS SELECT
 FROM moderators m
 JOIN users u
 ON m.user_id = u.id;
+
+-- tipo para quando 'INSERT_INTO_MODERATORS' é chamado.
+CREATE TYPE MODERATOR_INSERTION_RESULT AS ENUM (
+  'done',
+  'issuspended',
+  'notfound',
+  'alreadymoderator'
+);
+
+-- função que realiza checagens para manter integridade antes de adicionar à tabela.
+CREATE FUNCTION insert_into_moderators(IN p_id INTEGER) RETURNS moderator_insertion_result
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  exists BOOLEAN;
+  is_suspended BOOLEAN;
+  is_moderator BOOLEAN;
+BEGIN
+  SELECT EXISTS (
+    SELECT 1 FROM users AS u WHERE u.id = p_id
+  ) INTO exists;
+  IF NOT exists THEN
+    RETURN 'notfound';
+  END IF;
+  SELECT (current_status = 'suspended') INTO is_suspended
+  FROM users AS u WHERE p_id = u.id;
+  IF is_suspended THEN
+    RETURN 'issuspended';
+  END IF;
+  SELECT EXISTS (
+    SELECT 1 FROM moderators m WHERE m.user_id = p_id
+  ) INTO is_moderator;
+  IF is_moderator THEN
+    RETURN 'alreadymoderator';
+  END IF;
+  INSERT INTO moderators (user_id)
+  VALUES (p_id);
+  RETURN 'done';
+END;
+$$;
