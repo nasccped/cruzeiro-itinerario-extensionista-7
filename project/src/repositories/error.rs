2393dfd2235ca0900e9c0652969ad6e9f86b1a3d
Nasccped <pedro.main.tec@gmail.com>
2026-05-16 21:34:27 -0300

    chore: implement thiserror

--- project/src/repositories/error.rs
+++ project/src/repositories/error.rs
@@ -1,36 +1,35 @@
 #![allow(clippy::from_over_into)]
 use actix_web::HttpResponse;
 
-/// Quando ocorre um erro na operação (no banco de dados).
-pub struct DbOperationError {
-    error: sqlx::Error,
-    query: String,
+/// Quando ocorre um erro (no banco de dados) por meio dos repositories (no banco de dados).
+#[derive(thiserror::Error, Debug)]
+#[error(
+    "Erro ao executar query '{}' no banco de dados. Retornou:\n\
+    \n\
+    {}",
+    .on_query,
+    .err
+)]
+pub struct RepositoryError {
+    err: sqlx::Error,
+    on_query: String,
 }
 
-impl DbOperationError {
+impl RepositoryError {
     /// Retorna um [`DbOperationError`] a partir de um dador erro + query.
-    pub fn from_err_and_query(error: sqlx::Error, query: impl ToString) -> Self {
-        Self {
-            error,
-            query: query.to_string(),
-        }
+    pub fn from_err_and_query(err: sqlx::Error, query: impl ToString) -> Self {
+        let on_query = query.to_string();
+        Self { err, on_query }
+    }
+
+    /// Retorna o erro interno do [`DbOperationError`].
+    pub fn error(&self) -> &sqlx::Error {
+        &self.err
     }
 }
 
-impl Into<HttpResponse> for DbOperationError {
+impl Into<HttpResponse> for RepositoryError {
     fn into(self) -> HttpResponse {
         HttpResponse::InternalServerError().body(self.to_string())
-    }
-}
-
-#[allow(clippy::to_string_trait_impl)]
-impl ToString for DbOperationError {
-    fn to_string(&self) -> String {
-        format!(
-            "Erro ao executar query '{}' no banco.\n\
-            \n\
-            Erro retornado: {}",
-            self.query, self.error
-        )
     }
 }
