9d49fb354659b54d692ccaeb26cf5af8a93644d2
Nasccped <pedro.main.tec@gmail.com>
2026-05-17 11:56:43 -0300

    chore: add comments + moderators table/view

--- project/db-templates/default-schema.sql
+++ project/db-templates/default-schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE project_db;
 
 \c project_db;
 
+-- tabela para status de usuário.
 CREATE TABLE user_statuses (
   id          INT         PRIMARY KEY,
   status_name VARCHAR(20) NOT NULL UNIQUE
@@ -12,6 +13,7 @@ VALUES
   (1, 'Disponível'),
   (2, 'Suspenso'  );
 
+-- tabela para os usuários
 CREATE TABLE users (
   id             SERIAL      PRIMARY KEY    ,
   user_name      VARCHAR(50) NOT NULL UNIQUE,
@@ -25,12 +27,25 @@ CREATE TABLE users (
     ON DELETE SET NULL
 );
 
+-- tabela para os moderadores
 CREATE TABLE moderators (
-  id      SERIAL PRIMARY KEY ,
-  user_id INT UNIQUE NOT NULL,
+  id             SERIAL PRIMARY KEY                ,
+  user_id        INT UNIQUE NOT NULL               ,
+  moderator_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
   CONSTRAINT fk_users_user_id
     FOREIGN KEY (user_id)
     REFERENCES users(id)
     ON DELETE CASCADE
 );
+
+-- view para os moderadores
+CREATE VIEW moderators_view AS SELECT
+  m.id             as "moderator_id",
+  u.id             as "user_id"     ,
+  u.user_name      as "name"        ,
+  u.user_mail      as "mail"        ,
+  m.moderator_date as "since"
+FROM moderators m
+JOIN users u
+ON m.user_id = u.id;
