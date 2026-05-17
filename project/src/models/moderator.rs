31bbe1d66e6bc49a690c6593201caeabb5a3d1b9
Nasccped <pedro.main.tec@gmail.com>
2026-05-17 16:35:47 -0300

    chore: add moderator post models

--- project/src/models/moderator.rs
+++ project/src/models/moderator.rs
@@ -1,6 +1,6 @@
 use super::generic_date_time::GenericDateTime;
 use serde::{Deserialize, Serialize};
-use sqlx::prelude::FromRow;
+use sqlx::prelude::{FromRow, Type};
 
 /// Tipo que representa uma view de moderador.
 #[derive(Serialize, Deserialize, FromRow, Debug)]
@@ -15,4 +15,47 @@ pub struct ModeratorView {
     mail: String,
     /// Moderador desde:
     since: GenericDateTime,
+}
+
+/// Json esperado para [`crate::usecases::moderator::ModeratorUsecase::post_moderator`].
+#[derive(Serialize, Deserialize)]
+pub struct PostModeratorBody {
+    /// Id do usuário em questão.
+    pub user_id: i32,
+}
+
+/// Tabela retornada para a chamada sql da função `INSERT_INTO_MODERATORS`.
+#[derive(Debug, FromRow)]
+pub struct ModeratorInsertionReturnType {
+    /// Qual variante foi retornada.
+    pub result: ModeratorInsertionVariant,
+    /// Id do usuário na tentativa da operação.
+    pub user_id: i32,
+}
+
+impl ModeratorInsertionReturnType {
+    /// Se a operação teve sucesso.
+    pub fn is_ok(&self) -> bool {
+        self.result.is_ok()
+    }
+}
+
+#[derive(Type, Debug, PartialEq)]
+#[sqlx(type_name = "moderator_insertion_variant", rename_all = "lowercase")]
+pub enum ModeratorInsertionVariant {
+    /// Não existe usuário com o id especificado.
+    NotFound,
+    /// Usuário existe mas está suspenso.
+    IsSuspended,
+    /// Usuário já é um moderador.
+    AlreadyModerator,
+    /// Operação feita com sucesso.
+    Done,
+}
+
+impl ModeratorInsertionVariant {
+    /// Se a variante expressa sucesso.
+    fn is_ok(&self) -> bool {
+        *self == ModeratorInsertionVariant::Done
+    }
 }
