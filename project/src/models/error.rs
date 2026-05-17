966ec2402db87066262ae7431a9092ba4c75e194
Nasccped <pedro.main.tec@gmail.com>
2026-05-16 21:24:28 -0300

    chore: implement thiserror

--- project/src/models/error.rs
+++ project/src/models/error.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::from_over_into)]
 use actix_web::HttpResponse;
 use std::{
     any::{Any, type_name},
@@ -7,13 +6,22 @@ use std::{
 };
 
 /// Erro quando parse de struct falha.
-pub struct ModelParseError<Model: Any, Input: Debug> {
+#[derive(thiserror::Error, Debug)]
+#[error(
+    "Não foi possível deserializar o objeto `{}` a partir do input `{:?}`.\n\
+    \n\
+    Tentativa retorna seguinte erro: {}",
+    type_name::<Model>(),
+    .input,
+    .error
+)]
+pub struct ModelParseError<Model: Any + Debug, Input: Debug> {
     input: Input,
     error: sqlx::Error,
     _marker: PhantomData<Model>,
 }
 
-impl<Model: Any, Input: Debug> ModelParseError<Model, Input> {
+impl<Model: Any + Debug, Input: Debug> ModelParseError<Model, Input> {
     /// Cria um [`ModelParseError`] a partir de um [`sqlx::Error`] e um input.
     pub fn from_err_and_input(error: sqlx::Error, input: Input) -> Self {
         Self {
@@ -24,22 +32,8 @@ impl<Model: Any, Input: Debug> ModelParseError<Model, Input> {
     }
 }
 
-impl<Model: Any, Input: Debug> Into<HttpResponse> for ModelParseError<Model, Input> {
-    fn into(self) -> HttpResponse {
-        HttpResponse::UnprocessableEntity().body(self.to_string())
-    }
-}
-
-#[allow(clippy::to_string_trait_impl)]
-impl<Model: Any, Input: Debug> ToString for ModelParseError<Model, Input> {
-    fn to_string(&self) -> String {
-        format!(
-            "Não foi possível deserializar o objeto `{}` a partir do input `{:?}`.\n\
-            \n\
-            Tentativa retorna seguinte erro: {}",
-            type_name::<Model>(),
-            self.input,
-            self.error
-        )
+impl<Model: Any + Debug, Input: Debug> From<ModelParseError<Model, Input>> for HttpResponse {
+    fn from(val: ModelParseError<Model, Input>) -> Self {
+        HttpResponse::UnprocessableEntity().body(val.to_string())
     }
 }
