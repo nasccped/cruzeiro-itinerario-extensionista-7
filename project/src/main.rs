86a11ae09e9f4e7be54139676ce2e27ad8bb5886
Nasccped <pedro.main.tec@gmail.com>
2026-05-18 22:52:23 -0300

    fix: latest changes fix

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,26 +1,31 @@
-mod config;
+mod context;
 mod controllers;
 mod helpers;
 mod models;
 mod repositories;
 mod usecases;
 
-use actix_web::{HttpServer, web};
-use config::Config;
+use crate::helpers::{FatalPanic, HelperPanicable};
+use actix_web::{App, HttpServer};
+use context::Context;
+use helpers::Helper;
+
+/// Inicialize os recursos essencias da aplicação (logger, dotenv).
+fn init_core() {
+    Helper::setup_dotenv().ok_or_fatal_panic();
+    Helper::init_logger();
+}
 
 #[actix_web::main]
 async fn main() {
-    helpers::setup_dotenv();
-    helpers::init_logger();
-    let conf = web::Data::new(Config::new().await);
-    let clone = conf.clone();
-    let server = HttpServer::new(move || helpers::create_app(clone.clone()))
-        .bind((conf.get_server_url(), conf.get_server_port()))
-        .unwrap_or_else(|e| helpers::could_not_build_server(e));
-    log::info!(
-        "Servidor rodando em `{}:{}`",
-        conf.get_server_url(),
-        conf.get_server_port()
-    );
+    init_core();
+    let gctx = Context::new().await;
+    let clone = gctx.clone();
+    let (url, port) = (gctx.server_url.as_str(), gctx.server_port);
+    let server = HttpServer::new(move || App::new().configure(|sv| Helper::configure(sv, &clone)))
+        .bind((url, port))
+        .map_err(HelperPanicable::<()>::CouldNotBuildServer)
+        .ok_or_fatal_panic();
+    log::info!("Servidor rodando em `{}:{}`", url, port);
     server.run().await.unwrap()
 }
