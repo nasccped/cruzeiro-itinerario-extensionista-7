e69deca85658b5ad613b195026c68964985fe9d4
Nasccped <pedro.main.tec@gmail.com>
2026-05-18 22:49:07 -0300

    chore: add helper object

--- project/src/helpers.rs
+++ project/src/helpers.rs
@@ -1,97 +1,181 @@
+#![allow(non_snake_case)]
 use crate::{
-    config::Config,
+    Context,
     controllers::{HomeController, ModeratorController, UserController},
 };
 use actix_web::{
-    App, Error,
-    body::MessageBody,
-    dev::{ServiceFactory, ServiceRequest, ServiceResponse},
-    web,
+    FromRequest, Handler, Responder, Route,
+    web::{self, ServiceConfig},
 };
-use std::fmt::{Debug, Display};
+use std::{
+    any::{Any, type_name},
+    env::{self, VarError},
+    fmt::Debug,
+    marker::PhantomData,
+    str::FromStr,
+};
 
-/// Tenta executar [`dotenv::dotenv`]. Executa [`panic`] caso haja falha.
-pub fn setup_dotenv() {
-    dotenv::dotenv().unwrap_or_else(|e| panic!("Falha ao executar função `dotenv`: {e:?}"));
+/// Alias de função para criar rotas de requisições `get`.
+fn Get<F, Args>(handler: F) -> Route
+where
+    F: Handler<Args>,
+    Args: FromRequest + 'static,
+    F::Output: Responder + 'static,
+{
+    web::get().to(handler)
 }
 
-/// Inicializa o logger.
-pub fn init_logger() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("project=info"))
-        .init();
+/// Alias de função para criar rotas de requisições `post`.
+fn Post<F, Args>(handler: F) -> Route
+where
+    F: Handler<Args>,
+    Args: FromRequest + 'static,
+    F::Output: Responder + 'static,
+{
+    web::post().to(handler)
 }
 
-/// Função de [`panic`] para quando variável de ambiente não é definida.
-pub fn undefined_var_panic(name: impl Display) -> ! {
-    panic!("Variável de ambiente ${name} não definida");
+pub trait FatalPanic<OkType> {
+    /// Retorna o valor interno da variante [`Ok`] (ou executa o [`Err`] apropriado).
+    fn ok_or_fatal_panic(self) -> OkType;
 }
 
-/// Função de [`panic`] para quando variável de ambiente possui valor inválido.
-pub fn invalid_var_format_panic(name: impl Display, value: impl Display) -> ! {
-    panic!("Variável de ambiente ${name} possui valor inválido: {value}");
-}
-
-/// Quando a construção do [`actix_web::HttpServer`] retorna [`Err`].
-pub fn could_not_build_server(err: impl Debug) -> ! {
-    panic!("Não foi possível gerar o servidor: {err:?}");
-}
-
-/// Mensagem de [`panic`] quando não é possível conectar com o banco de dados.
-pub fn could_not_connect_to_db_panic<D: Debug>(err: D) -> ! {
-    panic!("Não foi possível connectar com o banco de dados => {err:?}");
-}
-
-/// Cria um novo [`App`] padrão para o projeto.
-pub fn create_app(
-    config: web::Data<Config>,
-) -> App<
-    impl ServiceFactory<
-        ServiceRequest,
-        Response = ServiceResponse<impl MessageBody>,
-        Config = (),
-        InitError = (),
-        Error = Error,
-    >,
-> {
-    let mut app = App::new()
-        .app_data(config.user_usecase.clone())
-        .app_data(config.moderator_usecase.clone());
-    let endpoints_and_routes = [
-        (
-            HomeController::get_app_home_endpoint(),
-            web::get().to(HomeController::app_home),
-        ),
-        (
-            UserController::get_users_endpoint(),
-            web::get().to(UserController::get_users),
-        ),
-        (
-            UserController::get_user_by_id_endpoint(),
-            web::get().to(UserController::get_user_by_id),
-        ),
-        (
-            UserController::post_user_endpoint(),
-            web::post().to(UserController::post_user),
-        ),
-        (
-            UserController::patch_user_endpoint(),
-            web::patch().to(UserController::patch_user),
-        ),
-        (
-            ModeratorController::get_moderators_endpoint(),
-            web::get().to(ModeratorController::get_moderators),
-        ),
-        (
-            ModeratorController::post_moderator_endpoint(),
-            web::post().to(ModeratorController::post_moderator),
-        ),
-        (
-            ModeratorController::delete_moderator_endpoint(),
-            web::delete().to(ModeratorController::delete_moderator),
-        ),
-    ];
-    for (endpoint, route) in endpoints_and_routes {
-        app = app.route(endpoint, route);
+impl<'a, AnyOk: Any, ParseError: Debug> FatalPanic<AnyOk>
+    for Result<AnyOk, HelperPanicable<'a, ParseError>>
+{
+    fn ok_or_fatal_panic(self) -> AnyOk {
+        match self {
+            Ok(ok) => ok,
+            Err(err) => panic!("{}", err.to_string()),
+        }
     }
-    app
+}
+
+/// Struct que opera as funções auxiliares.
+pub struct Helper {}
+
+/// Possíveis erros retornados ao executar funções do [`Helper`].
+#[derive(thiserror::Error, Debug)]
+pub enum HelperPanicable<'a, VarParseOutput: Debug> {
+    /// Quando a função de [`dotenv::dotenv`] falha.
+    #[error("Falha ao executar função `dotenv`: {:?}", .0)]
+    DotEnv(dotenv::Error),
+    /// Variável de ambiente não existe.
+    #[error("Variável de ambiente `{}` é indefinida!", .0)]
+    UndefinedEnvVar(&'a str),
+    /// Variável de ambiente não é unicode válido.
+    #[error("Variável de ambiente `{}` não é unicode válido!", .0)]
+    NonUnicodeEnvVar(&'a str),
+    /// Quando a variável de ambiente existe mas não é do tipo esperado.
+    #[error(
+        "Variável de ambiente `{}` não é um {} válido: {}",
+        .var_name,
+        type_name::<VarParseOutput>(),
+        .var_value
+    )]
+    CouldNotParseEnvVar {
+        /// Nome da variável.
+        var_name: &'a str,
+        /// Valor da variável.
+        var_value: String,
+        /// Phatom data para [`type_name`] printing.
+        out_type: PhantomData<VarParseOutput>,
+    },
+    /// Quando a construção do servidor falha.
+    #[error("Não foi possível construir o servidor http: {:?}", .0)]
+    CouldNotBuildServer(std::io::Error),
+    /// Quando há um erro durante a conexão com o banco de dados.
+    #[error("Não foi possível conectar com o banco de dados: {:?}", .0)]
+    CouldNotConnectToDB(sqlx::Error),
+}
+
+impl Helper {
+    /// Tenta executar [`dotenv::dotenv`]. Executa [`panic`] caso haja falha.
+    pub fn setup_dotenv<'a>() -> Result<(), HelperPanicable<'a, ()>> {
+        dotenv::dotenv()
+            .map(|_| ())
+            .map_err(HelperPanicable::DotEnv)
+    }
+
+    /// Inicializa o logger.
+    pub fn init_logger() {
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("project=info"))
+            .init();
+    }
+
+    /// Obtem a variável de ambiente como uma [`String`] (ou o [`HelperPanicable`] apropriado).
+    pub fn get_env_var<'a>(var_name: &'a str) -> Result<String, HelperPanicable<'a, ()>> {
+        env::var(var_name).map_err(|e| match e {
+            VarError::NotPresent => HelperPanicable::UndefinedEnvVar(var_name),
+            VarError::NotUnicode(_) => HelperPanicable::NonUnicodeEnvVar(var_name),
+        })
+    }
+
+    /// Faz _parsing_ de uma [`String`] para outro tipo. Caso falhe, retorna o [`HelperPanicable`]
+    /// apropriado.
+    pub fn parse_env_var<'a, Output: Debug + FromStr>(
+        var_name: &'a str,
+        var_value: String,
+    ) -> Result<Output, HelperPanicable<'a, Output>> {
+        var_value
+            .parse::<Output>()
+            .map_err(|_| HelperPanicable::CouldNotParseEnvVar {
+                var_name,
+                var_value,
+                out_type: PhantomData,
+            })
+    }
+
+    /// Configura o [`App`] por meio de um [`ServiceConfig`].
+    pub fn configure(service: &mut ServiceConfig, gctx: &Context) {
+        Self::add_app_data(service, gctx);
+        Self::add_home_routes(service);
+        Self::add_users_routes(service);
+        Self::add_moderators_routes(service);
+    }
+
+    /// Adiciona os `AppData` à configuração.
+    fn add_app_data(service: &mut ServiceConfig, gctx: &Context) {
+        service
+            .app_data(gctx.user_usecase.clone())
+            .app_data(gctx.moderator_usecase.clone());
+    }
+
+    /// Adiciona as rotas de [`HomeController`].
+    fn add_home_routes(service: &mut ServiceConfig) {
+        service.route(
+            HomeController::app_home_endpoint(),
+            Get(HomeController::app_home),
+        );
+    }
+
+    /// Adiciona as rotas de [`UserController`].
+    fn add_users_routes(service: &mut ServiceConfig) {
+        service
+            .route(
+                UserController::get_users_endpoint(),
+                Get(UserController::get_users),
+            )
+            .route(
+                UserController::get_user_by_id_endpoint(),
+                Get(UserController::get_user_by_id),
+            )
+            .route(
+                UserController::post_user_endpoint(),
+                Post(UserController::post_user),
+            );
+    }
+
+    /// Adiciona as rotas de [`ModeratorController`].
+    fn add_moderators_routes(service: &mut ServiceConfig) {
+        service
+            .route(
+                ModeratorController::get_moderators_endpoint(),
+                Get(ModeratorController::get_moderators),
+            )
+            .route(
+                ModeratorController::post_moderator_endpoint(),
+                Post(ModeratorController::post_moderator),
+            );
+    }
 }
