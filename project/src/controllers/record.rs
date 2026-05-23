56347636ae9ddb4afff49af6d6bc618d9cdf5808
Nasccped <pedro.main.tec@gmail.com>
2026-05-22 22:34:26 -0300

    fix: add usecase call

--- project/src/controllers/record.rs
+++ project/src/controllers/record.rs
@@ -1,5 +1,6 @@
 use super::_utils as utils;
-use actix_web::HttpResponse;
+use crate::usecases::records::RecordUsecase;
+use actix_web::{HttpResponse, web};
 
 /// Controller para os records.
 pub struct RecordController {}
@@ -11,9 +12,7 @@ impl RecordController {
     }
 
     /// Obtém a lista de records registrada no banco de dados.
-    pub async fn get_records() -> HttpResponse {
-        let err: Result<HttpResponse, HttpResponse> =
-            Err(HttpResponse::NotImplemented().body("Funcionalidade ainda não implementada!"));
-        utils::log_and_normalize(err, Self::get_records_endpoint())
+    pub async fn get_records(usecase: web::Data<RecordUsecase>) -> HttpResponse {
+        utils::log_and_normalize(usecase.get_records().await, Self::get_records_endpoint())
     }
 }
